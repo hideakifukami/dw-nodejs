f4fbf607fc368fa48cc01b5a4fe13da097dd13b0
hideakifukami <denys.fukami@gmail.com>
2023-08-11 11:19:59 -0300

    att

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,1 +1,13 @@
-console.log("Teste de NodeJs")
+// console.log("Teste de NodeJs")
+
+var n1 = 10
+var n2 = 2
+var adicao = n1 + n2
+var sub = n1 - n2
+var mult = n1 * n2
+var div = n1 / n2
+
+console.log("Adição: " + adicao)
+console.log("Subtração: " + sub)
+console.log("Multiplicação: " + mult)
+console.log("Divisão: " + div)
