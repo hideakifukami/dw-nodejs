36a09b3aa78e68c81b69d2908470b48114b505ea
hideakifukami <denys.fukami@gmail.com>
2023-08-18 10:55:20 -0300

    aula 03

--- projeto-express/app.js
+++ projeto-express/app.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
 
+app.use("/img",  express.static(__dirname + "/html/img"));
+
+
+
 app.get("/", function(req, res){
     res.send("Seja Bem-vindo ao NodeJS")
 })
@@ -9,10 +13,22 @@ app.get("/contato", function(req, res){
     res.send("Página de Contato")
 })
 
+app.get("/texto", function(req, res){
+    res.send("Página de Texto")
+})
+
+app.get("/index", function(req, res){
+    res.sendFile(__dirname + "/html/index.html")
+})
+
 app.get("/produtos/:item/:qtd", function(req, res){
     res.send("Item: " + req.params.item + "<br> Quantidade: " + req.params.qtd)
 })
 
+app.get("/produtos/:item", function(req, res){
+    res.send("Item: " + req.params.item)
+})
+
 app.listen(8081, function() {
-    console.log("Servidor Rodando na Porta 8081")
+    console.log("Servidor Rodando na Portinha do Nelsin")
 })
