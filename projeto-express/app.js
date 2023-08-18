aa35a4b8db08c4242147e3e4d9ca4052bc15d65d
hideakifukami <denys.fukami@gmail.com>
2023-08-18 09:41:26 -0300

    att

--- projeto-express/app.js
+++ projeto-express/app.js
@@ -5,6 +5,14 @@ app.get("/", function(req, res){
     res.send("Seja Bem-vindo ao NodeJS")
 })
 
+app.get("/contato", function(req, res){
+    res.send("Página de Contato")
+})
+
+app.get("/produtos/:item/:qtd", function(req, res){
+    res.send("Item: " + req.params.item + "<br> Quantidade: " + req.params.qtd)
+})
+
 app.listen(8081, function() {
     console.log("Servidor Rodando na Porta 8081")
 })
